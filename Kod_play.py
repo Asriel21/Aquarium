5174420f491b8fb9426bd89cbef8c13354dfa6e4
Asriel21 <67226590+Asriel21@users.noreply.github.com>
2020-06-26 20:26:10 +0300

    Update Kod_play.py

--- Kod_play.py
+++ Kod_play.py
@@ -8,7 +8,12 @@ win = pygame.display.set_mode((700, 700))
 #Название
 pygame.display.set_caption("AQUARIUM")
 
+x_M = 0
+y_M = 0
+
+class Monika
+
 run=True
 #Запускаем цикл
 while run == True :
-  
+  x_M=y_M
